0bf12a7997475df821b14fc756482dcc27e8ea90
GooieJS <conradklek@gmail.com>
2022-03-28 22:34:20 -0500

    submit

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -3,8 +3,45 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+const testShow = {
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: [],
+        }
+    ]
+}
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders without errors with no props', async () => {
+    render(<Display />);
+});
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders Show component when the button is clicked ', () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    render(<Display />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+    });
+});
+
+test('renders show season options matching your data when the button is clicked', () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    const displayFunc = jest.fn();
+    render(<Display displayFunc={displayFunc} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled();
+    });
+});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,31 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 1,
+    name: 'Episode 1',
+    image: null,
+    season: 1,
+    number: 1,
+    summary: 'summary',
+    runtime: 60,
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => {
+    render(<Episode episode={testEpisode} />);
+});
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={testEpisode} />);
+    const summary = screen.queryByText(/summary/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/summary/i);
+});
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={testEpisode} />);
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+    expect(image).toBeInTheDocument();
+    expect(image).toBeTruthy();
+});
